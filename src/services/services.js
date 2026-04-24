52b05eacd96b8f31052dd381ec37aac3e8c02ca1
Javier Urbina Rathgeb <urbinarathgeb@gmail.com>
2026-04-23 21:38:31 -0400

    refactor: funciones getData y setData

--- src/services/services.js
+++ src/services/services.js
@@ -5,8 +5,13 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 const dataPath = path.join(__dirname, '../data/data.json');
 
+export const getData = () => {
+	const data = fs.readFileSync(dataPath, 'utf-8');
+	return JSON.parse(data);
+}
 
-export const readFile = () => fs.readFileSync(dataPath, 'utf-8');
+export const setData = (data) => {
+	fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
+}
